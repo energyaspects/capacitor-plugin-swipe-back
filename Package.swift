f42ad6a1a99cfd5561ec831278fb537b29875f46
rajsharmaea <raj.sharma@energyaspects.com>
2025-07-11 13:07:50 +0530

    ios: update spm config

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "EnergyaspectsEaCapacitorSwipeBackPlugin",
     platforms: [
-        .iOS(.v14)
+        .iOS("14.0")
     ],
     products: [
         .library(
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", .exact("7.0.0"))
     ],
     targets: [
         .target(
